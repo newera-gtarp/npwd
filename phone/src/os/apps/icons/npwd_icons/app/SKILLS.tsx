1170dfe9afc006e75adaff44b96e95c81ebe40a6
Erik Bigler <erikbigler@gmail.com>
2022-03-19 11:56:05 -0500

    fix: tweak skills app

--- phone/src/os/apps/icons/npwd_icons/app/SKILLS.tsx
+++ phone/src/os/apps/icons/npwd_icons/app/SKILLS.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { Psychology } from '@mui/icons-material';
 
-const SkillsIcon: React.FC = () => <Psychology fontSize="large" />;
+const SkillsIcon: React.FC = () => (
+  <div
+    style={{
+      width: '3.25vw',
+      height: '3.25vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: 'red',
+      borderRadius: '50%',
+    }}
+  >
+    <Psychology fontSize="large" />;
+  </div>
+);
 
 export default SkillsIcon;
